cb515c5baa928e474c935b3eb4402cd0374f76e9
ArielSalvadorDev <arielsalvadormejia@gmail.com>
2019-01-03 19:20:56 -0600

    agregar estilos inline y con archivo css externo

--- src/playlist/components/media.js
+++ src/playlist/components/media.js
@@ -2,16 +2,31 @@
 //import React from 'react';
 //segunda forma desfragmentando para traer unicamente la propiedad y no todo el objeto o clase
 import React, {Component} from 'react';
+//importamos un archivo css
+import './media.css';
+
 //class Media extends React.Component {
 
 //en vez de traer el objeto y su propiedad unicamente traemos su propiedad    
 class Media extends Component {
     render() {
+        // const styles = {
+        //     container: {
+        //         color: '#44546b',
+        //         cursor: 'pointer',
+        //         marginRight: 15,
+        //         display: 'inline-block',
+        //         verticalAlign: 'top',
+        //         width: 260,
+        //         border: '1px solid red'
+        //     }
+        // }
         return (
             <div>
-                <div>
+                <div className="container">
+                {/* <div  style={styles.container}> */}
                     <img 
-                        src="" 
+                        src="images/cover/bitcoin.png"
                         alt="" 
                         width={260}
                         height={160}
