a08f1a19cf14c5f9eb841671ce74e1750f9bd27d
ArielSalvadorDev <arielsalvadormejia@gmail.com>
2019-01-05 19:31:45 -0600

    videoplayer parte 1

--- src/pages/containers/home.js
+++ src/pages/containers/home.js
@@ -4,7 +4,8 @@ import Categories from '../../categories/components/categories'
 import Related from '../../related/components/related'
 import ModalContainer from '../../widgets/containers/modal-container'
 import Modal from '../../widgets/components/modal'
-import HandleError from '../../widgets/containers/handle-error';
+import HandleError from '../../widgets/containers/handle-error'
+import VideoPlayer from '../../player/containers/video-player'
 class Home extends Component {
     state = {
         modalVisible: false,
@@ -27,6 +28,7 @@ class Home extends Component {
             <HandleError>
                 <HomeLayout>
                     <Related />
+                    <VideoPlayer/>
                     <Categories openModal={this.openModal} categories={this.props.data.categories} />
                     {/* este es un if que valida el valor boolean del state */}
                     {/* modalVisible solo es un boolean inventado pero podria ser cualquier cosa no es que sea un metodo de react o algo asi solo es una propiedad del objeto state que si se cumple muestra los bloques del ModalContainer si no siemplemente no muestra nada xq no los carga  */}
