31dea4382adf5b0ab1b4d1b8cf868ae924dec68a
ArielSalvadorDev <arielsalvadormejia@gmail.com>
2019-01-05 21:47:18 -0600

    video player agregando componentes con componente de iconos y funcion onClick toogle

--- src/player/containers/video-player.js
+++ src/player/containers/video-player.js
@@ -2,12 +2,27 @@ import React, { Component } from 'react'
 import VideoPlayerLayout from '../components/video-player-layout'
 import Video from '../components/video'
 import Title from '../components/title'
+import PlayPause from '../components/play-pause'
 class VideoPlayer extends Component {
+    state = {
+        pause: true,
+    }
+    togglePlay = ev => {
+        this.setState({
+            //esto es lo mismo que decir que es lo contrario a la propiedad pause del objeto state
+            //asi en una sola funcion va cambiar de true a false constantemente xq cambia al contrario del state actual
+            pause: !this.state.pause
+        })
+    }
     render() {
         return(
             <VideoPlayerLayout>
                 <Title 
                     title="Esto es un video chido!"
+                />
+                <PlayPause 
+                    pause={this.state.pause}
+                    handleClick={this.togglePlay}
                 />
                 <Video 
                     autoplay={true}
