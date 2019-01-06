9109ee799dd277cfb0cd6c91a4df18816eaa8767
ArielSalvadorDev <arielsalvadormejia@gmail.com>
2019-01-05 20:58:17 -0600

    Video player parte 2 mas componentes "quemados"

---
+++ src/player/components/video.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react'
+import './video.css'
+class Video extends Component {
+    render() {
+        return(
+            <div className="Video">
+                <video 
+                    src={this.props.src}
+                    autoPlay ={this.props.autoplay}
+                />
+            </div>
+        )
+    }
+}
+export default Video
